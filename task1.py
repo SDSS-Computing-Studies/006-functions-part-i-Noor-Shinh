891262ceecb49cffeeed4fa36ceeb34d0a87b44b
Noor-Shinh <71406898+Noor-Shinh@users.noreply.github.com>
2020-10-07 13:00:35 -0700

    Update task1.py

--- task1.py
+++ task1.py
@@ -5,11 +5,20 @@ def sum(a,b):
     # a : float 
     # b : float
     # return value: returns the sum of the 2 numbers
-
+    x=(a+b)
+    return x
+    
 
 
 #this should return a value of 7
 x = sum(3,4)
-
+print(x)
 #this should return a value of 12.5
 y = sum(11,1.5)
+print(y)
+
+a=float(input("Enter a number"))
+b=float(input("Enter a number"))
+
+c=sum(a,b)
+print(c)
