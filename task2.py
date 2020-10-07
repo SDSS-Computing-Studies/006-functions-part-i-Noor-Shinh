ae48f53d07295ad09d9cd87f661f0b07c9e84dfe
Noor-Shinh <71406898+Noor-Shinh@users.noreply.github.com>
2020-10-07 13:00:01 -0700

    Update task2.py

--- task2.py
+++ task2.py
@@ -6,3 +6,13 @@ The input is a list.
 The return value is the largest value in the list
 (2 points)
 """
+def largest(l):
+    l.sort()
+    x=(l[-1])
+    return x
+
+y=largest([1,15,88,2])
+print(y)
+
+c=largest([15,99,2])
+print(c)
