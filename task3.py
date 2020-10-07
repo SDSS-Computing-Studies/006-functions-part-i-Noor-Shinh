e25b6947d1c55832a54a002e724804b469a1dadf
Noor-Shinh <71406898+Noor-Shinh@users.noreply.github.com>
2020-10-07 13:06:24 -0700

    Update task3.py

--- task3.py
+++ task3.py
@@ -6,3 +6,11 @@ The return value is the sum of all the numbers in the list
 added together
 (2 points)
 """
+def perimeter(x):
+    b=0
+    for i in x:
+        b=b+i
+    return b
+
+c=perimeter([1,15,16])
+print(c)
