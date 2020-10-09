d74e5d7bce0d2907ad3d66944d83762bac6adabd
Noor-Shinh <71406898+Noor-Shinh@users.noreply.github.com>
2020-10-08 17:38:02 -0700

    Update problem2.py

--- problem2.py
+++ problem2.py
@@ -8,10 +8,16 @@ Note that the coordinates should be signed (positive or negative) floats
 (2 points)
 """
 
+import math 
+
 def distance(a,b):
-    import math
-    c=math.dist(a,b)
-    return c
+    x=(a[0]-b[0])
+    x=math.pow(x,2)
+    y=(a[1]-b[1])
+    y=math.pow(y,2)
+    z=(x+y)
+    z=math.sqrt(z)
+    return z
 
 d=round(distance([-3,2.2],[1,2]),3)
 print(d)
